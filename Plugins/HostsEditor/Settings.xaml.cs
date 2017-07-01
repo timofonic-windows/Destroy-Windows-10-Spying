1c174cc65f73f80034fcba096be8f6688edf4895
Nummer <Nummer@users.noreply.github.com>
2017-07-01 18:11:00 +0300

    update settings to XAML

---
+++ Plugins/HostsEditor/Settings.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HostsEditor
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : UserControl
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(@"Test Button in settings clicked");
+        }
+    }
+}
