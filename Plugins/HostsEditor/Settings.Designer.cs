c0bfd0da526e2df84021e574ea40c61bd3373363
Nummer <Nummer@users.noreply.github.com>
2017-07-01 18:08:21 +0300

    архитектура плагина

---
+++ Plugins/HostsEditor/Settings.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace HostsEditor
+{
+    partial class Settings
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Location = new System.Drawing.Point(72, 71);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(152, 51);
+            this.btnTest1.TabIndex = 0;
+            this.btnTest1.Text = "Test settings btn";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnTest1);
+            this.Name = "Settings";
+            this.Size = new System.Drawing.Size(317, 362);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest1;
+    }
+}
